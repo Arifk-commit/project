fc9c78cf6a6ee115a9f3388aa55baf3dd2e802b9
Arifk-comit <khanarifshamshul@gmail.com>
2025-03-07 07:42:18 +0530

    hello

--- chatbot/src/components/Input.jsx
+++ chatbot/src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const styles = {
     container : {
@@ -14,30 +14,77 @@ const styles = {
     width: '70vw'
     },
     inputBox: {
-        padding: '10px',
-        margin: '10px',
-        border: '2px solid #424242',
+        // padding: '10px',
+        // margin: '10px',
+        // border: '2px solid #424242',
         borderRadius: '5px',
         width: '250px',
         fontSize: '16px',
       }
   };
+  
 
-const Input = () => {
-  return (
+  
+  const[isOpen,setIsOpen] = useState(false)
+
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  
+  const Input = () => {
+
+
+
+    return (
     // <div className=' mt-6 '>
     //     <div className='border-2 w-200 p-3 min-h-20 rouded-full rounded-full'>
     //         <input type="text" placeholder='What do you want to know' className='border-hidden width-200'/>
     //     </div>
     // </div>
+
     <div style={styles.container}> 
          <input 
          type="text" 
          placeholder= "What do you want to know"
          style={styles.inputBox}
          />
-    </div>
+         <div
+          className='flex justify-between pt-3 mt-2'>
+          <img src="src\assets\upload_icon.png" alt="" 
+          className='h-7 cursor-pointer'
+          />
+
+          <div className='dropdown'>
+          
+          <div onClick={toggleDropdown} 
+          
+          className='flex items-center p-2   text-center' >
+            <img src="
+            src\assets\arrow_drop_down.png" alt="" 
+            className='h-5 w-7 cursor-pointer '
+            />
+
+              </div>
+            
+             {isOpen &&(
+              <ul className="dropdown-menu">
+                <li className="dropdown-item">Option 1</li>
+                <li className="dropdown-item">Option 2</li>
+                <li className="dropdown-item">Option 3</li>
+              </ul>
+            )}
+          
+            
+            
+            </div>
+            </div>
+         </div>
+   
+    
   )
+
 
  
 }
--- chatbot/src/components/Navbar.jsx
+++ chatbot/src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import './Navbar.css'
 
 const navbar = () => {
     return (
-        <div className='flex items-center justify-between'>
+        <div className='flex items-center justify-between p-0 m-0'>
             <h1 className='font-bold '>Grok</h1>
             <div className='flex gap-4'>
-                <button className='flex items-center gap-1 border-1 bg-white text-black px-3 py-1 rounded-full cursor-pointer'>
+                <button
+                    className='flex items-center gap-1 border-1 bg-white text-black px-3 py-1 rounded-full cursor-pointer'>
                     <img src="src/assets/add-user.png" alt="" className='h-5' />
                     SignUp
-                    </button>
-                <button className='cursor-pointer'>SignIn</button>
+                </button>
+                <button
+                    className='cursor-pointer rounded-full  px-3 py-1'>
+                    SignIn
+                </button>
             </div>
         </div>
     )
